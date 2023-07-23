36a99aa81c854074cc75d66957617d02be4b0ca7
jac2206 <arango773@gmail.com>
2023-07-23 14:40:31 -0500

    Mejorar consulta

--- src/infraestructure/repositories/sqlserver-repository.js
+++ src/infraestructure/repositories/sqlserver-repository.js
@@ -10,7 +10,10 @@ export const getCompaniesDB = async () => {
 };
 
 export const getCompanyDB = async (partnerCode) => {
+  // return await client.query(
+  //   `SELECT CO.id, CO.partnerCode, CO.nit, CO.verificationCode, CO.name, CO.comercialName, CO.shortName ,CO.categoryCode , CAT.categoryName AS category, CO.email, CO.webSite, CO.typeCLM, CO.pointsMoneyRatioCLM, CO.billingTypeCLM, CO.accumulationPeriodCLM, CO.categoriesJSON, CO.currency, CO.address, CO.country, CO.regionCode, CO.regionName, CO.cityName, CO.cityCode, CO.neighbourhood, CO.geolocalizationJSON, CO.isParther, CO.isClient, CO.status, CO.initDate, CO.endDate, CO.lastUpdate, CO.additionalInfoJSON FROM dbo.Company AS CO INNER JOIN dbo.Category AS CAT ON CAT.categoryCode = CO.categoryCode WHERE CO.partnerCode = ${'\''+partnerCode+'\''}`
+  // );
   return await client.query(
-    `SELECT CO.id, CO.partnerCode, CO.nit, CO.verificationCode, CO.name, CO.comercialName, CO.shortName ,CO.categoryCode , CAT.categoryName AS category, CO.email, CO.webSite, CO.typeCLM, CO.pointsMoneyRatioCLM, CO.billingTypeCLM, CO.accumulationPeriodCLM, CO.categoriesJSON, CO.currency, CO.address, CO.country, CO.regionCode, CO.regionName, CO.cityName, CO.cityCode, CO.neighbourhood, CO.geolocalizationJSON, CO.isParther, CO.isClient, CO.status, CO.initDate, CO.endDate, CO.lastUpdate, CO.additionalInfoJSON FROM dbo.Company AS CO INNER JOIN dbo.Category AS CAT ON CAT.categoryCode = CO.categoryCode WHERE CO.partnerCode = ${'\''+partnerCode+'\''}`
+    `SELECT CO.id, CO.partnerCode, CO.nit, CO.verificationCode, CO.name, CO.comercialName, CO.shortName ,CO.categoryCode , CAT.categoryName AS category, CO.email, CO.webSite, CO.typeCLM, CO.pointsMoneyRatioCLM, CO.billingTypeCLM, CO.accumulationPeriodCLM, CO.categoriesJSON, CO.currency, CO.address, CO.country, CO.regionCode, CO.regionName, CO.cityName, CO.cityCode, CO.neighbourhood, CO.geolocalizationJSON, CO.isParther, CO.isClient, CO.status, CO.initDate, CO.endDate, CO.lastUpdate, CO.additionalInfoJSON FROM dbo.Company AS CO INNER JOIN dbo.Category AS CAT ON CAT.categoryCode = CO.categoryCode WHERE CO.partnerCode = '${partnerCode}'`
   );
 };
